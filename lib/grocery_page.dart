a5385685415ca087491fe44dee40b963e014733e
Julian Koripamo <jkoripamo@gmail.com>
2024-08-07 21:17:12 +0100

    input field and button

--- lib/grocery_page.dart
+++ lib/grocery_page.dart
@@ -8,8 +8,75 @@ class GroceryPage extends StatefulWidget {
 }
 
 class _GroceryPageState extends State<GroceryPage> {
+  final TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Grocery Pal',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      height: 35,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              controller: _controller,
+                              decoration: InputDecoration(
+                                contentPadding: const EdgeInsets.only(
+                                    top: 0, left: 10, right: 10),
+                                filled: true,
+                                fillColor: Colors.grey[200],
+                                border: const OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 33,
+                            child: MaterialButton(
+                              onPressed: () {},
+                              color: Colors.blue[300],
+                              child: const Padding(
+                                padding: EdgeInsets.only(left: 8, right: 8),
+                                child: Text(
+                                  'Add Item',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
