57eaec2c9cd6d994e368d14b2a8a4e60b4c1d0b8
Julian Koripamo <jkoripamo@gmail.com>
2024-08-09 02:10:54 +0100

    alert dialog to update item

--- lib/components/item_tile.dart
+++ lib/components/item_tile.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 class ItemTile extends StatefulWidget {
   final String item;
-  final VoidCallback onTap;
+  final ValueChanged<String> onUpdate;
   final VoidCallback onPressed;
-  ItemTile({
+  const ItemTile({
     super.key,
     required this.item,
     required this.onPressed,
-    required this.onTap,
+    required this.onUpdate,
   });
 
   @override
@@ -19,54 +19,84 @@ class _ItemTileState extends State<ItemTile> {
   bool ticked = false;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Checkbox(
-                value: ticked,
-                onChanged: (value) {
-                  setState(() {
-                    ticked = !ticked;
-                  });
-                },
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Checkbox(
+              value: ticked,
+              onChanged: (value) {
+                setState(() {
+                  ticked = !ticked;
+                });
+              },
+            ),
+            const SizedBox(width: 10),
+            Text(
+              widget.item,
+              style: TextStyle(
+                fontSize: 16,
+                letterSpacing: 1,
+                decoration:
+                    ticked ? TextDecoration.lineThrough : TextDecoration.none,
               ),
-              const SizedBox(width: 10),
-              Text(
-                widget.item,
-                style: TextStyle(
-                  fontSize: 16,
-                  letterSpacing: 1,
-                  decoration:
-                      ticked ? TextDecoration.lineThrough : TextDecoration.none,
-                ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                _showUpdateDialog(context);
+              },
+              icon: const Icon(
+                Icons.edit,
+                color: Colors.blueGrey,
               ),
-            ],
+            ),
+            IconButton(
+              onPressed: widget.onPressed,
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  void _showUpdateDialog(BuildContext context) {
+    final TextEditingController editController =
+        TextEditingController(text: widget.item);
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: TextField(
+            controller: editController,
           ),
-          Row(
-            children: [
-              IconButton(
-                onPressed: widget.onTap,
-                icon: const Icon(
-                  Icons.edit,
-                  color: Colors.blueAccent,
-                ),
-              ),
-              IconButton(
-                onPressed: widget.onPressed,
-                icon: const Icon(
-                  Icons.delete,
-                  color: Colors.red,
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                widget.onUpdate(editController.text);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Update'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
